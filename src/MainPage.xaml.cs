a3f57a55b4aa064e718ba4fb788aad199a036f60
Rich Turner <richturn@microsoft.com>
2016-09-16 13:05:02 -0700

    Initial Checkin

---
+++ src/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CreateSymlink
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        enum SYMBOLIC_LINK_FLAG : int { File = 0x00, Directory = 0x01 };
+
+        [DllImport("kernel32.dll")]
+        static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, SYMBOLIC_LINK_FLAG dwFlags);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void CreateSymlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool result = CreateSymbolicLink(
+                @"C:\hosts.txt", 
+                @"C:\Windows\System32\Drivers\Etc\Hosts", 
+                SYMBOLIC_LINK_FLAG.File);
+
+            if (true == result)
+            {
+                CreateSymlinksResultMessageBox.Text = "Symlink Created";
+            }
+            else
+            {
+                CreateSymlinksResultMessageBox.Text = "Symlink Creation Failed";
+
+            }
+        }
+    }
+}
